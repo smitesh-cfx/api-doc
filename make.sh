1377faa21e984dfab18851f26591f1041f2ab37b
Smitesh Maka <smiteshkumar.maka@cloudfabrix.com>
2025-02-25 15:57:22 +0530

    updated

--- make.sh
+++ make.sh
@@ -1,11 +1,1 @@
-#!/bin/bash
-
-echo "Getting started"
-
-# Bundle docs into zero-dependency HTML file
-npx redoc-cli bundle openapi.json && \
-mv redoc-static.html index.html && \
-echo "Changed name from redoc-static.html to index.html" && \
-# Add favicon
-sed -i '7 i \ \ <link rel="icon" type="image/png" href="images/favicon.png"/>' index.html && \
-echo -e "\nDone!"
+#!/bin/bashecho "Getting started"# Bundle docs into zero-dependency HTML filenpx redoc-cli bundle openapi.json && \mv redoc-static.html index.html && \echo "Changed name from redoc-static.html to index.html" && \# Inject custom CSSsed -i '6 i \ \ <link rel="stylesheet" type="text/css" href="styles.css"/>' index.html && \# Add faviconsed -i '7 i \ \ <link rel="icon" type="image/png" href="images/favicon.png"/>' index.html && \echo -e "\nDone!"
