0d9796c58cad39fcbf698bf6023280dd57903a95
smitesh-cfx <121931356+smitesh-cfx@users.noreply.github.com>
2025-02-26 02:46:46 +0530

    Update make.sh

--- make.sh
+++ make.sh
@@ -4,8 +4,8 @@ echo "Setting up Docusaurus"
 
 # Check if Docusaurus is already initialized
 if [ ! -f "package.json" ]; then
-  echo "Docusaurus project not found, initializing..."
-  npx create-docusaurus@latest . classic --skip-install
+  echo "Docusaurus project not found! Make sure it's initialized."
+  exit 1
 fi
 
 # Install dependencies
