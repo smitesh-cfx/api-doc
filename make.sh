eab83a9b28ec9159113c04fead09768da0ff18a0
smitesh-cfx <121931356+smitesh-cfx@users.noreply.github.com>
2025-02-26 02:40:00 +0530

    Update make.sh

--- make.sh
+++ make.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
+
 echo "Setting up Docusaurus"
-npx create-docusaurus@latest api-doc classic
-cd api-doc
+
+# Create Docusaurus project in non-interactive mode
+npx create-docusaurus@latest api-doc classic --use npm --skip-install
+
+# Change directory
+cd api-doc || exit
+
+# Install dependencies
 npm install
+
+# Build the site
 npm run build
